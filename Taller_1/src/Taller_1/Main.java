f81839524832ae23e4b603af1e460239455a6503
victo <victo@MISH>
2026-03-23 21:04:30 -0300

    Cambios:
    1.- Creciación de varaibles las cuales emplearé:
    
    	- Scanner:
    
    		Generado para usar cuando cree el menú, en lugar de hacer
    		decenas de Scanners, reutilizo el mismo y me ahorro
    		lineas de código.
    
    	- Arrellos varios:
    
    		Generados para almacenar la información extraida
    		de los txt.
    
    2.- Generación de la lectura de archivos:
        Se generaron 3 métodos, cada uno con una tarea principal.
    
        -  RellenarArreglo:
    
        	Encargado de leer el txt correspodiente para luego
        	comunicarse con 2 métodos los cuales almacenarán
        	los datos
    
        -   AlmacenarUsuario y AlmacenarRegistros:
    
        	Encargado de aplicar un split a la linea y guardar
        	su información en sus respectivos arrelglos.
    
    "No olvidar las librerias importadas para el correcto funcionamiento
     del código"

--- Taller_1/src/Taller_1/Main.java
+++ Taller_1/src/Taller_1/Main.java
@@ -1,10 +1,122 @@
 package Taller_1;
 
+//Importar librerias para axtivar las funciones
+import java.util.Scanner;
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*Generar Scanner Global, este lo emplearé durante todo el código
+		 *para ahorrar espacio usandolo como una variable en los métodos*/
+		Scanner scanner = new Scanner(System.in);
+		
+		/*Generar Arreglos los cuales me servirán como una memoria de cada dato
+		 *presente en los txt con un largo de 300 según aparece en la pauta*/
+		
+		//Usuarios
+		String[] UsuarioID = new String[300];
+		String[] UsuarioContraseña = new String[300];
+		
+		//Registros
+		String[] RegistroID = new String[300];
+		String[] RegistroFecha = new String[300];
+		int[] RegistroHoras = new int[300];
+		String[] RegistroActividad = new String[300];
+		
+		/*Generar método el cual lea y rellene los Arreglos mediante lectura del txt de manera ordenada*/
+		int a = 0; int b = 0;
+		RellenarArreglo("Usuarios.txt", UsuarioID, UsuarioContraseña, RegistroID, RegistroFecha, RegistroHoras, RegistroActividad, a);
+		RellenarArreglo("Registros.txt", UsuarioID, UsuarioContraseña, RegistroID, RegistroFecha, RegistroHoras, RegistroActividad, b);
+		
+		
+		
+		//cerrar scanner como una buena práctica :D
+		scanner.close();
+	
 	}
+	
+	//Método generado para imprimir bonito :D
+	public static void Print(String texto) {
+		System.out.println(texto);
+	}
+	
+	
+	/* ------------------ Sección generada para lectura y almacenamiento de los datos de los txt ------------------*/
 
+	/*Método empleado para generar una lectura de archivo en base al archivo que se le entregue
+	 *comunicandose con otros método para almacenar cada dato en su Arreglo correspondiente*/
+	private static void RellenarArreglo(String archivo, String[] usuarioID, String[] usuarioContraseña, 
+			String[] registroID, String[] registroFecha, int[] registroHoras, String[] registroActividad, int posición) {
+		
+		File file = new File(archivo);
+		
+		try {
+			
+			Scanner lector = new Scanner(file);
+			
+			while(lector.hasNextLine()) {
+				
+				String linea = lector.nextLine();
+				
+				if(archivo.equals("Usuarios.txt")) {
+					
+					AlmacenarUsuario(linea, usuarioID, usuarioContraseña, posición);
+					posición++;
+					
+				}
+				
+				else if (archivo.equals("Registros.txt")) {
+					
+					AlmacenarRegistros(linea, registroID, registroFecha, registroHoras, registroActividad, posición);
+					posición++;
+					
+				}
+			}
+			
+				lector.close();
+				
+		}
+	
+		catch(Exception e){
+			
+			Print("\nArchivo no encontrado\n");
+			
+		}
+	}
+	
+	
+	/*Método el cual mediante la entrega de una linea de texto, desglozará esta
+	 *y guardará su información en su arreglo correspondiente*/
+	public static void AlmacenarUsuario(String linea, String[] usuarioID, String[] usuarioContraseña, int posición) {
+
+		String[] Partes = linea.split(";");
+		
+		usuarioID[posición] = Partes[0];
+		usuarioContraseña[posición] = Partes[1];
+		
+	}
+	
+	/*Método el cual mediante la entrega de una linea de texto, desglozará esta
+	 *y guardará su información en su arreglo correspondiente*/
+	private static void AlmacenarRegistros(String linea, String[] registroID, String[] registroFecha, 
+			int[] registroHoras, String[] registroActividad, int posición) {
+		
+		String[] Partes = linea.split(";");
+		
+		registroID[posición] = Partes[0];
+		registroFecha[posición] = Partes[1];
+		registroHoras[posición] = Integer.valueOf(Partes[2]);
+		registroActividad[posición] = Partes[3];
+		
+	}
+	
+	/*----------------------------------------------------------------------------------------------------------------------*/
+	
+	
+	
+	
+	
 }
+
